821e9b7d6aade45f3efe59811b35ae2450250fde
Jiang Chen <chen.apaul@gmail.com>
2016-12-12 13:22:01 +0800

    add ert test for MACRO: backwards

--- test/simple-nano-test.el
+++ test/simple-nano-test.el
@@ -1,3 +1,9 @@
+;;; simple-nano-test --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
 
 (ert-deftest sum-test ()
   (should (= 10 (sum 3 7)))
@@ -6,3 +12,10 @@
 (ert-deftest multiply-test ()
   (should (= 21 (multiply 3 7)))
   (should-not (= 10 (multiply 3 7))))
+
+(ert-deftest backwards-test ()
+  (should (= 10 (backwards (5 5 sum))))
+  (should (= 50 (backwards (5 5 2 multiply)))))
+
+
+;;; simple-nano-test.el ends here
